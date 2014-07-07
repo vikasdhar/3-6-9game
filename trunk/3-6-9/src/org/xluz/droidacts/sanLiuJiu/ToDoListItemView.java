24330ed3763e6a62f88f20ece2481b8365021ac6
socsweeper@gmail.com <socsweeper@gmail.com@f7ddebd2-6603-948f-b6d2-f0b5662c7e1f>
2014-07-07 01:50:18 +0000

    Added a 9x9 grid

--- trunk/3-6-9/src/org/xluz/droidacts/sanLiuJiu/ToDoListItemView.java
+++ trunk/3-6-9/src/org/xluz/droidacts/sanLiuJiu/ToDoListItemView.java
@@ -5,7 +5,9 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ToDoListItemView extends TextView {
 	private Paint marginPaint;
@@ -31,14 +33,67 @@ public class ToDoListItemView extends TextView {
 		marginPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	    marginPaint.setColor(0xFF0000FF);
 	    linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	    linePaint.setColor(0xEEF8E0A0);
+	    linePaint.setColor(0x90FF00FF);
 
 	}
 	@Override 
 	public void onDraw (Canvas canvas) {
-		super.onDraw(canvas);
+		float xx=getMeasuredWidth()-5;
+		float yy=getMeasuredHeight()-5;
+		// Draw ruled lines
+	    canvas.drawLine(0, 0, 0, yy, linePaint);
+	    canvas.drawLine(0, 0, xx, 0, linePaint);
+	   
+	    canvas.drawLine(xx/9, 0, xx/9, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9,
+	                       getMeasuredWidth(), getMeasuredHeight()/9,
+	                       linePaint);
+	    canvas.drawLine(xx/9*2, 0, xx/9*2, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*2,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*2,
+	                       linePaint);
+	    canvas.drawLine(xx/9*3, 0, xx/9*3, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*3,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*3,
+	                       linePaint);
+	    canvas.drawLine(xx/9*4, 0, xx/9*4, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*4,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*4,
+	                       linePaint);
+	    canvas.drawLine(xx/9*5, 0, xx/9*5, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*5,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*5,
+	                       linePaint);
+	    canvas.drawLine(xx/9*6, 0, xx/9*6, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*6,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*6,
+	                       linePaint);
+	    canvas.drawLine(xx/9*7, 0, xx/9*7, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*7,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*7,
+	                       linePaint);
+	    canvas.drawLine(xx/9*8, 0, xx/9*8, yy, linePaint);
+	    canvas.drawLine(0, getMeasuredHeight()/9*8,
+	                       getMeasuredWidth(), getMeasuredHeight()/9*8,
+	                       linePaint);
+	    canvas.drawLine(xx/9*9, 0, xx/9*9, yy, linePaint);
+	    canvas.drawLine(0, yy/9*9,
+	                       getMeasuredWidth(), yy/9*9,
+	                       linePaint);
+	   
+	   
 	}
-	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// Get the type of action this event represents
+		int actionPerformed = event.getAction();
+		float x0=event.getX();
+		float y0=event.getY();
+//		Toast.makeText(this, "U have touched the board",Toast.LENGTH_LONG).show();
+		// Return true if the event was handled.
+		return true;
+	}
+
 }
 
 
