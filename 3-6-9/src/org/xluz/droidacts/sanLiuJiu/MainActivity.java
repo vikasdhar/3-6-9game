eb4a556dafcbbd4f7f827368b0e6f4ca56b0c130
cctransluminance <cctransluminance@gmail.com>
2014-08-07 07:16:19 +0000

    scoring partially works.

--- 3-6-9/src/org/xluz/droidacts/sanLiuJiu/MainActivity.java
+++ 3-6-9/src/org/xluz/droidacts/sanLiuJiu/MainActivity.java
@@ -8,30 +8,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private  TextView p1, p2, s1, s2;
 	GamePlay G0;
-	//private int Bcolor;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        CreateMenu(menu);
-        return true;
-    }
-    
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		return MenuChoice(item);
-	}
-	
-	private void CreateMenu(Menu menu)
+    private void CreateMenu(Menu menu)
 	{
 		MenuItem mnu1 = menu.add(0, 0, 0,"Intro");
 		{
@@ -51,8 +31,8 @@ public class MainActivity extends Activity {
 		}
 		menu.add (0, 3, 3, "Settings");
 	}
-	
-	private boolean MenuChoice(MenuItem item)
+
+    private boolean MenuChoice(MenuItem item)
 	{ 
         switch (item.getItemId()) {
         case 0:
@@ -63,8 +43,7 @@ public class MainActivity extends Activity {
 			introDialog.show();
             return true;
         case 1:
-            Toast.makeText(this, "You vs. AI (to be done)",
-                Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "U vs AI (to be done)", Toast.LENGTH_LONG).show();
             return true;
         case 2:
             Toast.makeText(this, "2-players", Toast.LENGTH_LONG).show();
@@ -74,9 +53,12 @@ public class MainActivity extends Activity {
             GameBoard bd = (GameBoard)findViewById(R.id.editText3);
             bd.setGame0(G0);
             bd.setGameState(2);
+            // clear scores
+			s1.setText(Integer.toString(G0.scores1));
+			s2.setText(Integer.toString(G0.scores2));
             return true;
         case 3:
-            Toast.makeText(this, "You clicked on Item 4",
+            Toast.makeText(this, "Nothing to set (yet)",
                 Toast.LENGTH_LONG).show();
             return true;
 		case R.id.action_settings:
@@ -88,6 +70,35 @@ public class MainActivity extends Activity {
 			return true;
         }
         return false;
+	}
+    
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        CreateMenu(menu);
+        return true;
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		return MenuChoice(item);
+	}
+
+	@Override
+	protected void onStart() {
+		p1 = (TextView)findViewById(R.id.editText1);
+		p2 = (TextView)findViewById(R.id.editText2);
+		s1 = (TextView)findViewById(R.id.textView2);
+		s2 = (TextView)findViewById(R.id.textView3);
+		super.onStart();
 	}
 
 	@Override
@@ -102,24 +113,18 @@ public class MainActivity extends Activity {
 				Toast.makeText(this, "Game ended", Toast.LENGTH_LONG).show();
 			}
 			else if(s%2==1) {
-				TextView t1 = (TextView)findViewById(R.id.editText1);
 				// highlight player-1
-				t1.setBackgroundColor(0xFFFFFF66);
-				TextView t2 = (TextView)findViewById(R.id.editText2);
-				t2.setBackgroundColor(0xFFFFFFFF);
+				p1.setBackgroundColor(0xFFFFFF66);
+				p2.setBackgroundColor(0xFFFFFFFF);
 				// update score
-				TextView s2 = (TextView)findViewById(R.id.textView3);
 				s2.setText(Integer.toString(G0.scores2));
 			}
 			else if(s%2==0) {
-				TextView t1 = (TextView)findViewById(R.id.editText1);
-				t1.setBackgroundColor(0xFFFFFFFF);
+				p1.setBackgroundColor(0xFFFFFFFF);
 				// update score
-				TextView s1 = (TextView)findViewById(R.id.textView2);
 				s1.setText(Integer.toString(G0.scores1));
-				TextView t2 = (TextView)findViewById(R.id.editText2);
 				// highlight player-2
-				t2.setBackgroundColor(0xFFFFFF66);				
+				p2.setBackgroundColor(0xFFFFFF66);				
 			}
 		}
 		return super.onTouchEvent(event);
